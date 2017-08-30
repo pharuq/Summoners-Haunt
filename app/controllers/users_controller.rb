46db95b6d8bd963f7d3d188c373ff9c2abc5ea8d
Ryoma Shindo <pharuq2@hotmail.co.jp>
2017-08-30 23:18:02 +0900

    login dake

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,72 @@
+class UsersController < ApplicationController
+  before_action :logged_in_user,  only: [:edit, :update, :destroy,
+                                        :friends]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
+    # @diaries = @user.diaries.paginate(page: params[:page])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile Update"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
+  def friends
+    @user  = User.find(params[:id])
+    @users = @user.friends.paginate(page: params[:page])
+    render 'show_friends'
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :role, :profile, :password, :password_confirmation)
+    end
+
+    # beforeアクション
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_url unless current_user?(@user)
+    end
+
+    # 管理者かどうか確認
+    def admin_user
+      redirect_to root_url unless current_user.admin?
+    end
+end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'pharuq2@hotmail.co.jp'
   layout 'mailer'
 end
