71db845f28d6305feab6914e255399a55711a53a
Ryoma Shindo <pharuq2@hotmail.co.jp>
2018-01-02 22:12:02 +0900

    deploy2

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,11 @@
+_proj_path = "#{File.expand_path("../..", __FILE__)}"
+_proj_name = File.basename(_proj_path)
+_home = ENV.fetch("HOME") { "/home/centos" }
+
+pidfile "#{_home}/run/#{_proj_name}.pid"
+bind "unix://#{_home}/run/#{_proj_name}.sock"
+directory _proj_path
+
 # workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
