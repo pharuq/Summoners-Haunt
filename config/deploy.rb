8507c77e01f25dcbe94d54c5a0e392f5fb97f4bd
Ryoma Shindo <pharuq2@hotmail.co.jp>
2018-01-13 23:19:46 +0900

    open4

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,7 @@ append :linked_files, "config/database.yml", "config/secrets.yml", ".rbenv-vars"
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 # append :linked_dirs, "bin", "log", "tmp/pids", "tmp/cache", "tmp/sockets", "bundle", "public/system", "public/assets"
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "bundle", "public/system", "public/assets"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "bundle", "public/system", "public/assets", "public/uploads"
 set :bundle_binstubs, -> { shared_path.join('bin') }
 
 # Default value for default_env is {}
